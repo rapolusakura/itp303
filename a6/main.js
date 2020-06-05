b39d8edffb519fa523d58b6b87ee7496184dffd9
Sakura Rapolu <rapolusakura@gmail.com>
2020-06-05 16:31:53 -0700

    started a6

---
+++ a6/main.js
@@ -0,0 +1,31 @@
+		
+		// // Use this array to keep track of contacts
+		// let contacts = [];
+		// var form = document.getElementById("contact-form"); 
+		// var showAllContactsBtn = document.getElementById("show-all-btn"); 
+		// var showThreeContactsBtn = document.getElementById("show-three-btn"); 
+		// var deleteAllBtn = document.getElementById("delete-btn"); 
+		// var contactList = document.getElementById("contact-list"); 
+
+		// var addContact = function(event) {
+		// 	var fname = document.getElementById("fname").value; 
+		// 	var lname = document.getElementById("lname").value; 
+		// 	var email = document.getElementById("email").value; 
+
+		// 	if(fname !== "" && lname !== "" &&  email !== "") {
+		// 		var contact = {
+		// 			fname: fname, 
+		// 			lname: lname, 
+		// 			email: email
+		// 		}; 
+
+		// 		contacts.push(contact); 
+		// 	}
+		// 	event.preventDefault();
+		// }
+
+		// form.addEventListener("submit", addContact, true);
+		// showAllContactsBtn.addEventListener('click', showAllContacts); 
+		// deleteAllBtn.addEventListener('click', deleteAllContacts); 
+		// showThreeContactsBtn.addEventListener('click', showThreeContacts); 
+
