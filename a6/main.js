ebecba2b692a9c5e43f5b098c6ff0dac3c17c54d
Sakura Rapolu <rapolusakura@gmail.com>
2020-06-11 21:46:11 -0700

    displays movies

--- a6/main.js
+++ a6/main.js
@@ -1,31 +1,39 @@
 		
-		// // Use this array to keep track of contacts
-		// let contacts = [];
-		// var form = document.getElementById("contact-form"); 
-		// var showAllContactsBtn = document.getElementById("show-all-btn"); 
-		// var showThreeContactsBtn = document.getElementById("show-three-btn"); 
-		// var deleteAllBtn = document.getElementById("delete-btn"); 
-		// var contactList = document.getElementById("contact-list"); 
+// Use this array to keep track of contacts
+var form = document.getElementById("search-form"); 
 
-		// var addContact = function(event) {
-		// 	var fname = document.getElementById("fname").value; 
-		// 	var lname = document.getElementById("lname").value; 
-		// 	var email = document.getElementById("email").value; 
+function displayMovie(item, index) {
+	var movies = document.getElementById("movies"); 
+	var div = document.createElement("div"); 
+	div.innerHTML = item.title + " " + item.release_date + " https://image.tmdb.org/t/p/w500" + item.poster_path;  
+	movies.appendChild(div); 
+}
 
-		// 	if(fname !== "" && lname !== "" &&  email !== "") {
-		// 		var contact = {
-		// 			fname: fname, 
-		// 			lname: lname, 
-		// 			email: email
-		// 		}; 
+var ajax = function(event) {
+	$.get("https://api.themoviedb.org/3/movie/now_playing?api_key=c4f2023ec81f7fa2b3508c0379329db0&language=en-US", function(data, status){
+	  console.log(data.results.length); 
+	  data.results.forEach(displayMovie);
+	//	  $("#weather").html("Today's weather in Los Angeles: " + temp + " " + desc + ". Feels like " + feels_like);
+	});
+	event.preventDefault();
+}
 
-		// 		contacts.push(contact); 
-		// 	}
-		// 	event.preventDefault();
-		// }
+form.addEventListener("submit", ajax, true);
 
-		// form.addEventListener("submit", addContact, true);
-		// showAllContactsBtn.addEventListener('click', showAllContacts); 
-		// deleteAllBtn.addEventListener('click', deleteAllContacts); 
-		// showThreeContactsBtn.addEventListener('click', showThreeContacts); 
 
+
+// var showInTheaters = () => {
+// 	var xhttp = new XMLHttpRequest();
+// 	xhttp.onreadystatechange = function() {
+// 	    if (this.readyState == 4 && this.status == 200) {
+// 	       // Typical action to be performed when the document is ready:
+// 	       document.getElementById("movies").innerHTML = xhttp.responseText;
+// 	    }
+// 	};
+// 	xhttp.open("GET", "./style.css", true);
+// 	xhttp.send();	
+// }
+
+
+
+// window.onload(showInTheaters()); 
